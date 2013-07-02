0a2773b45c669e7643f6f824d7476d9b8c891ec9
James Michael DuPont <JamesMikeDuPont@gmail.com>
2013-07-02 07:20:11 -0500

    maplight update

--- families/rootstrikers-wikia_family.py
+++ families/rootstrikers-wikia_family.py
@@ -11,6 +11,18 @@ class Family(family.Family):
         self.langs = {
             'en': 'rootstrikers.wikia.com',
         }
+        self.namespaces[4] = {            'en': 'Rootstrikers Wiki',        }
+        self.namespaces[5] = {            'en': 'Rootstrikers Wiki talk',        }
+        self.namespaces[110] = {            'en': 'Forum',        }
+        self.namespaces[111] = {            'en': 'Forum talk',        }
+        self.namespaces[500] = {            'en': 'User blog',        }
+        self.namespaces[501] = {            'en': 'User blog comment',        }
+        self.namespaces[502] = {            'en': 'Blog',        }
+        self.namespaces[503] = {            'en': 'Blog talk',        }
+        self.namespaces[1200] = {            'en': 'Message Wall',        }
+        self.namespaces[1201] = {            'en': 'Thread',        }
+        self.namespaces[1202] = {            'en': 'Message Wall Greeting',        }
+
         # Wikimedia wikis all use "bodyContent" as the id of the <div>
         # element that contains the actual page content; change this for
         # wikis that use something else (e.g., mozilla family)
@@ -20,7 +32,7 @@ class Family(family.Family):
     # Which version of MediaWiki is used? REQUIRED
     def version(self, code):
         # Replace with the actual version being run on your wiki
-        return '1.14'
+        return '1.19.7'
 
     def scriptpath(self, code):
         return ''
